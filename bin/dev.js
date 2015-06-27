798df0f0f96871cafd1e76b7b9b12107c7d19b88
kvnlnt <kvnlnt@yahoo.com>
2015-06-26 21:41:25 -0700

    New Statical layout

--- bin/dev.js
+++ bin/dev.js
@@ -1,25 +1,31 @@
-// modules
+// TOC
+// -----------------------
+// MODULES
+// SHORTCUTS
+// BUILDERS
+// CLEANERS
+// WATCHERS
+// MENU
+
+// MODULES
+var terminalMenu = require('terminal-menu');
 var browserSync = require("browser-sync").create();
 var del = require('del');
 var frontMatter = require('front-matter');
 var fs = require('fs');
-var sass = require('node-sass');
 var packageJson = require('../package.json');
 var swig = require('swig');
 var swigPiece = new swig.Swig({ varControls: ['{{@piece', '}}'], cache:false });
 var swigPart = new swig.Swig({ varControls: ['{{@part', '}}'], cache:false });
 var swigPage = new swig.Swig({ varControls: ['{{@page', '}}'], cache:false });
 var swigProperty = new swig.Swig({ varControls: ['{{@property', '}}'], cache:false });
-var uglifyCss = require("uglifyCss");
-var uglifyJs = require("uglify-js");
-
-// TOC
-// -----------------------
-// SHORTCUTS
-// HELPERS 
-// BUILD SCRIPTS
-// CLEANERS
-// WATCHERS
+var babel = require("babel-core");
+var postcss = require('postcss');
+var customMedia = require("postcss-custom-media")
+var autoprefixer = require('autoprefixer');
+var cssnext = require('cssnext');
+var csswring = require('csswring');
+var util = require('./lib/util.js');
 
 // SHORTCUTS
 var patterns = packageJson.build.patterns;
@@ -28,54 +34,29 @@ var pages = packageJson.build.pages;
 var parts = packageJson.build.parts;
 var pieces = packageJson.build.pieces;
 
-// HELPERS
-var getFileContents = function(file) {
-    return fs.existsSync(file) ? fs.readFileSync(file).toString() : '';
-};
-
-var ensureFilePath = function(file, index){
-
-    var path = file.split('/'); path.pop();
-    var len = path.length-1;
-    var directory = path.splice(0, index+1).join('/');
-
-    // if folder not exists, create it
-    var exists = fs.existsSync(directory);
-    if (!exists) {
-        console.log('create directory: ', directory);
-        fs.mkdirSync(directory);
-    }
-
-    // if not last element, increment and recurse
-    if(index < len){
-        index += 1;
-        ensureFilePath(file, index);
-    }
-};
-
-// BUILD SCRIPTS 
+// BUILDERS
 var buildJs = function(){
-    var patternScripts = patterns.scripts.map(function(script){
-        return './src/patterns/scripts/' + script + ".js";
+    var patternScripts = patterns.elements.map(function(script){
+        return './src/patterns/' + script + ".js";
     });
 
-    var propertyScripts = property.scripts.map(function(script){
+    var propertyScripts = property.elements.map(function(script){
         return './src/property/' + script + ".js";
     });
 
-    var pageScripts = pages.map(function(script){
+    var pageScripts = pages.elements.map(function(script){
         return './src/pages/' + script + ".js";
     });
 
-    var partScripts = parts.map(function(script){
+    var partScripts = parts.elements.map(function(script){
         return './src/parts/' + script + ".js";
     });
 
-    var pieceScripts = pieces.map(function(script){
+    var pieceScripts = pieces.elements.map(function(script){
         return './src/pieces/' + script + ".js";
     });
 
-    var scripts = patternScripts
+    var files = patternScripts
         .concat(propertyScripts)
         .concat(pageScripts)
         .concat(partScripts)
@@ -84,33 +65,48 @@ var buildJs = function(){
             return fs.existsSync(script);
         });
 
-    var js = uglifyJs.minify(scripts, { outSourceMap: "scripts.js.map", sourceMapIncludeSources:true, mangle:false });
-    fs.writeFileSync('./build/scripts.js', js.code);
-    fs.writeFileSync('./build/scripts.js.map', js.map);
+    var concatedFiles = files.map(function(file){
+        return util.getFileContents(file);
+    }).join('');
+
+    var result = babel.transform(concatedFiles,{ 
+        compact:true,
+        sourceMaps:true,
+        sourceFileName:"maps/scripts.js",
+        sourceMapTarget:'scripts.js'
+    });
+
+    // hackily add in sourceMappingUrl due to babel js api not supporting it
+    var compiledCode = result.code + '\n//# sourceMappingURL=' + 'scripts.js.map';
+
+    util.cliMessage("BUILT", "./build/scripts.js");
+    util.cliMessage("BUILT", "./build/scripts.js.map");
+    fs.writeFileSync('./build/scripts.js', compiledCode);
+    fs.writeFileSync('./build/scripts.js.map', JSON.stringify(result.map));
 };
 
 var buildCss = function(){
-    var patternStyles = patterns.styles.map(function(style){
-        return './src/patterns/styles/' + style + ".scss";
+    var patternStyles = patterns.elements.map(function(style){
+        return './src/patterns/' + style + ".css";
     });
 
-    var propertyStyles = property.styles.map(function(style){
-        return './src/property/' + style + ".scss";
+    var propertyStyles = property.elements.map(function(style){
+        return './src/property/' + style + ".css";
     });
 
-    var pageStyles = pages.map(function(style){
-        return './src/pages/' + style + ".scss";
+    var pageStyles = pages.elements.map(function(style){
+        return './src/pages/' + style + ".css";
     });
 
-    var partStyles = parts.map(function(style){
-        return './src/parts/' + style + ".scss";
+    var partStyles = parts.elements.map(function(style){
+        return './src/parts/' + style + ".css";
     });
 
-    var pieceStyles = pieces.map(function(style){
-        return './src/pieces/' + style + ".scss";
+    var pieceStyles = pieces.elements.map(function(style){
+        return './src/pieces/' + style + ".css";
     });
 
-    var styles = patternStyles
+    var files = patternStyles
         .concat(propertyStyles)
         .concat(pageStyles)
         .concat(partStyles)
@@ -119,110 +115,162 @@ var buildCss = function(){
             return fs.existsSync(style);
         });
 
-    var concatedFiles = styles.map(function(file){
-        return getFileContents(file);
+    var concatedFiles = files.map(function(file){
+        return util.getFileContents(file);
     }).join('');
 
-    var sassCompiled = sass.renderSync({data: concatedFiles});
-    var css = uglifyCss.processString(sassCompiled.css.toString());
-    fs.writeFileSync('./build/styles.css', css);
+    postcss([customMedia(), autoprefixer(), cssnext(), csswring()])
+    .process(concatedFiles, { from:'maps/styles.css', to:'styles.css', map:{inline:false} })
+    .then(function (result) {
+        util.cliMessage('BUILT', './build/styles.css');
+        util.cliMessage('BUILT', './build/styles.css.map');
+        fs.writeFileSync('./build/styles.css', result.css);
+        fs.writeFileSync('./build/styles.css.map', result.map);
+    });
 };
 
 var buildHtml = function(items, path, swigFn, callback) {
     try {
 
         items.forEach(function(item) {
-            var jstFile = getFileContents(path + item + ".jst");
-            var ymlFile = getFileContents(path + item + ".yml");
+            var jstFile = util.getFileContents(path + item + ".jst");
+            var ymlFile = util.getFileContents(path + item + ".yml");
             var htmlFile = path + item + ".html";
             var data = frontMatter(ymlFile);
             var swigCompile = swigFn.compile(jstFile);
             data.attributes.content = data.body;
             var compiled = swigCompile(data.attributes);
             fs.writeFileSync(htmlFile, compiled);
-            console.log('compile:', htmlFile);
+            util.cliMessage('BUILT', htmlFile);
         });
 
         if(callback) callback();
 
     } catch (e) {
-        console.log(e);
+        util.cliMessage(e);
     }
 };
 
 var buildHtmlProperty = function(){
 
     // swig and copy all pages
-    pages.forEach(function(page){
+    pages.elements.forEach(function(page){
         var htmlFile = './src/pages/' + page + ".html";
         var targetFile = './build/' + page + ".html";
-        var htmlContents = getFileContents(htmlFile);
+        var htmlContents = util.getFileContents(htmlFile);
         var swigCompile = swigProperty.compile(htmlContents);
         var compiled = swigCompile(property);
         var dir = targetFile.split('/');
-        ensureFilePath(targetFile,1);
+        util.ensureFilePath(targetFile,1);
         fs.writeFileSync(targetFile, compiled);
-        console.log('build:', targetFile);
+        util.cliMessage('BUILT', targetFile);
     });
 };
 
 var buildHtmlPages = function(callback) {
-    buildHtml(pages, './src/pages/', swigPage, buildHtmlProperty);
+    buildHtml(pages.elements, './src/pages/', swigPage, buildHtmlProperty);
 };
 
 var buildHtmlParts = function(callback) {
-    buildHtml(parts, './src/parts/', swigPart, buildHtmlPages);
+    buildHtml(parts.elements, './src/parts/', swigPart, buildHtmlPages);
 };
 
 var buildHtmlPieces = function() {
-    buildHtml(pieces, './src/pieces/', swigPiece, buildHtmlParts);
+    buildHtml(pieces.elements, './src/pieces/', swigPiece, buildHtmlParts);
 };
 
 // CLEANERS
-var cleanHtml = function() {
+var cleanHtml = function(andBuild) {
     del(['./src/**/*.html', './build/**/*.html'], function(err, paths) {
-        console.log('Deleted files/folders:\n', paths.join('\n'));
-        buildHtmlPieces();
+        for(path in paths){
+            util.cliMessage('DELETED', paths[path], 'yellow');
+        }
+        if(andBuild) buildHtmlPieces();
     });
 };
 
-var cleanJs = function(){
-    del(['./build/*.js'], function(err, paths) {
-        console.log('Deleted files/folders:\n', paths.join('\n'));
-        buildJs();
+var cleanJs = function(andBuild){
+    del(['./build/*.{js,js.map}'], function(err, paths) {
+        for(path in paths){
+            util.cliMessage('DELETED', paths[path], 'yellow');
+        }
+        if(andBuild) buildJs();
     });
 };
 
-var cleanCss = function(){
-    del(['./build/*.css'], function(err, paths) {
-        console.log('Deleted files/folders:\n', paths.join('\n'));
-        buildCss();
+var cleanCss = function(andBuild){
+    del(['./build/*.{css,css.map}'], function(err, paths) {
+        for(path in paths){
+            util.cliMessage('DELETED', paths[path], 'yellow');
+        }
+        if(andBuild) buildCss();
     });
 };
 
-var cleanAll = function(){
-    cleanHtml();
-    cleanJs();
-    cleanCss();
+var cleanAll = function(andBuild){
+    cleanHtml(andBuild);
+    cleanJs(andBuild);
+    cleanCss(andBuild);
 };
-
-cleanAll();
-
 
 // WATCHERS
+var watchAll = function(){
+    // watch dev to rebuild files on change
+    browserSync.watch("./src/property/**/*.{jst,yml,layout}").on("change", buildHtmlProperty);
+    browserSync.watch("./src/patterns/**/*.{layout}").on("change", buildHtmlPages);
+    browserSync.watch("./src/pages/**/*.{jst,yml}").on("change", buildHtmlPages);
+    browserSync.watch("./src/parts/**/*.{jst,yml}").on("change", buildHtmlParts);
+    browserSync.watch("./src/pieces/**/*.{jst,yml}").on("change", buildHtmlPieces);
+    browserSync.watch("./src/+(patterns|property|pages|parts|pieces)/**/*.js").on("change", buildJs);
+    browserSync.watch("./src/+(patterns|property|pages|parts|pieces)/**/*.css").on("change", buildCss);
+    browserSync.watch("./package.json").on("change", cleanAll);
 
-// watch dev to rebuild files on change
-browserSync.watch("./src/property/**/*.{jst,yml,layout}").on("change", buildHtmlProperty);
-browserSync.watch("./src/patterns/**/*.{layout}").on("change", buildHtmlPages);
-browserSync.watch("./src/pages/**/*.{jst,yml}").on("change", buildHtmlPages);
-browserSync.watch("./src/parts/**/*.{jst,yml}").on("change", buildHtmlParts);
-browserSync.watch("./src/pieces/**/*.{jst,yml}").on("change", buildHtmlPieces);
-browserSync.watch("./src/+(patterns|property|pages|parts|pieces)/**/*.js").on("change", buildJs);
-browserSync.watch("./src/+(patterns|property|pages|parts|pieces)/**/*.{scss,css}").on("change", buildCss);
-browserSync.watch("package.json").on("change", cleanAll);
+    // watch build folder to reload on changes
+    browserSync.watch("./build/**/*").on("change", browserSync.reload);
 
-// watch build folder to reload on changes
-browserSync.watch("./build/**/*").on("change", browserSync.reload);
+    // launch it
+    browserSync.init({ server: "./build/"});
+};
 
-// Now init the Browsersync server
-browserSync.init({ server: "./build/"});
+// MENU
+var menu = terminalMenu({ width: 29, x: 4, y: 2 });
+menu.reset();
+menu.write('STATICAL\n');
+menu.write('-------------------------\n');
+menu.add('LAUNCH');
+menu.add('BUILD');
+menu.add('CLEAN');
+menu.add('EXIT');
+
+menu.on('select', function (label) {
+    switch(label) {
+        case 'LAUNCH':
+            util.cliMessage('DEV', "Launching", "yellow");
+            cleanAll(true);
+            watchAll();
+            menu.close();
+            break;
+        case 'BUILD':
+            util.cliMessage('DEV', "Building", "yellow");
+            buildHtmlPieces(); // this kicks of build hierarchy
+            buildJs();
+            buildCss();
+            break;
+        case 'CLEAN':
+            util.cliMessage('DEV', "Cleaning", "yellow");
+            cleanAll(false);
+            break;
+        case 'EXIT':
+            util.cliMessage('MENU', "Exited", "yellow");
+            menu.close();
+            break;
+    }
+});
+
+process.stdin.pipe(menu.createStream()).pipe(process.stdout);
+ 
+process.stdin.setRawMode(true);
+menu.on('close', function () {
+    process.stdin.setRawMode(false);
+    process.stdin.end();
+});
---
+++ bin/scaffold.js
@@ -0,0 +1,67 @@
+// MODULES
+var inquirer = require("inquirer");
+var fs = require('fs');
+var util = require('./lib/util.js');
+
+var questions = [
+    {
+        name: 'type',
+        type: 'list',
+        message: 'What would you like to scaffold?',
+        choices: ['PIECE', 'PART', 'PAGE', 'EXIT'],
+        default: 'PIECE',
+        filter: function(input) {
+            if(input === 'EXIT') {
+                process.exit();
+            } else {
+                return input;
+            }
+        }
+    },
+
+    {
+        name:'entity',
+        type: 'input',
+        message: "What's the name of this object?",
+        default: ''
+    }
+];
+
+inquirer.prompt(questions, function( answers ) {
+    switch(answers.type) {
+        case 'PIECE':
+            var root = 'pieces';
+            break;
+        case 'PART':
+            var root = 'parts';
+            break;
+        case 'PAGE':
+            var root = 'pages';
+            break;
+    }
+
+    // create vars
+    var jst = './src/' + root + '/' + answers.entity + '.jst';
+    var js  = './src/' + root + '/' + answers.entity + '.js';
+    var css = './src/' + root + '/' + answers.entity + '.css';
+    var yml = './src/' + root + '/' + answers.entity + '.yml';
+
+    // get package.json and update it
+    var packageJson = JSON.parse(fs.readFileSync('./package.json', { encoding: 'utf8' }));
+    packageJson.build[root].push(answers.entity);
+    var updates = {}; 
+    updates.build = {};
+    updates.build[root] = packageJson.build[root];
+    util.updateJSONpreserveFormat('./package.json', updates);
+
+    // create files
+    if (!fs.existsSync(jst)) {
+        util.ensureFilePath(jst, 0);
+        fs.writeFileSync(jst,'');
+        fs.writeFileSync(js,'');
+        fs.writeFileSync(css,'');
+        fs.writeFileSync(yml,'');
+    } else {
+        util.cliMessage('ALERT', answers.entity + ' already exist', 'red');
+    }
+});});
